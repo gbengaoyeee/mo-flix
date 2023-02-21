8dfb12450a4a0334f5e8f9fa10afa4819ea8778a
gbengaoyeee <24610110+gbengaoyeee@users.noreply.github.com>
2023-02-21 16:08:58 -0500

    more

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { CssBaseline } from '@mui/material'
+import { Route, Router, Routes   } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <CssBaseline />
+      <main>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/movies" element={<h1>Movies</h1>} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
