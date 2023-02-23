0b5dbbc20c751cf4ba3e20abe40281088ce6bf50
gbengaoyeee <24610110+gbengaoyeee@users.noreply.github.com>
2023-02-23 12:46:39 -0500

    including redux

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export default configureStore({
+    reducers: {
+        // Add your reducers here
+        
+    }
+})
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material'
+import { useGetMoviesQuery } from '../../services/tmdb'
+import { useSelector } from 'react-redux'
 
 const Movies = () => {
+  const { data, error, isLoading } = useGetMoviesQuery()
   return (
     <div>Movies</div>
   )
